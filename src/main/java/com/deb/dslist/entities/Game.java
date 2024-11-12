cd7e0cbc71f24c14a3e29d65ad31164eabbbb4c3
Moises Felix <moises012felix@gmail.com>
2024-11-12 10:29:32 -0300

    Game seed

--- src/main/java/com/deb/dslist/entities/Game.java
+++ src/main/java/com/deb/dslist/entities/Game.java
@@ -21,21 +21,25 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String paltforms;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game(){}
 
-	public Game(Long id, String title, Integer year, String genre, String paltforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.paltforms = paltforms;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -74,12 +78,12 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPaltforms() {
-		return paltforms;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPaltforms(String paltforms) {
-		this.paltforms = paltforms;
+	public void setPlatforms(String paltforms) {
+		this.platforms = paltforms;
 	}
 
 	public Double getScore() {
