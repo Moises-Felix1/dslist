13655639c06b9ce570c832ebf0a7ca081ad8c59a
Moises Felix <moises012felix@gmail.com>
2024-11-13 11:03:37 -0300

    Sql, Projection, Get games by lists

--- src/main/java/com/deb/dslist/controllers/GameListController.java
+++ src/main/java/com/deb/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.deb.dslist.dto.GameListDTO;
+import com.deb.dslist.dto.GameMinDTO;
 import com.deb.dslist.services.GameListService;
+import com.deb.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -16,6 +19,10 @@ public class GameListController {
 
 	@Autowired
 	private GameListService gameListService;
+	
+	@Autowired
+	private GameService gameService;
+
 
 	@GetMapping
 	public List<GameListDTO> findAll() {
@@ -23,4 +30,9 @@ public class GameListController {
 		return result;
 	}
 
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId){
+		var result = gameService.findByList(listId);
+		return result;
+	}
 }
--- src/main/java/com/deb/dslist/services/GameService.java
+++ src/main/java/com/deb/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.deb.dslist.dto.GameDTO;
 import com.deb.dslist.dto.GameMinDTO;
 import com.deb.dslist.entities.Game;
+import com.deb.dslist.projections.GameMinProjection;
 import com.deb.dslist.repositories.GameRepository;
 
 @Service
@@ -28,4 +29,11 @@ public class GameService {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
 }
